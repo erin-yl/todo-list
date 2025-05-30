61e7fea3c2039b012fff6a0452d80aa6b4fa5953
erin-yl <erinyinling@gmail.com>
2025-05-29 17:37:50 -0700

    Add MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,63 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true, // Clean the dist folder before each build
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist", // Serve content from the dist directory
-    open: true, // Automatically open the browser
-    hot: true, // Enable Hot Module Replacement
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader", // Use Babel to transpile JavaScript
-          options: {
-            presets: ["@babel/preset-env"], // Use modern JavaScript features
+module.exports = (env, argv) => {
+  const isProd = argv.mode === "production";
+
+  return {
+    mode: isProd ? "production" : "development",
+    entry: "./src/index.js",
+    output: {
+      filename: "[name].[contenthash].js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    devtool: isProd ? "source-map" : "inline-source-map",
+    devServer: {
+      static: "./dist",
+      open: true,
+      hot: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
+        {
+          test: /\.css$/i,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        inject: "body",
+      }),
+      ...(isProd
+        ? [
+          new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css",
+          }),
+        ]
+        : []),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
-      inject: "body", // Inject the script tag at the end of the body
-    }),
-  ],
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+      },
+      runtimeChunk: "single",
+    },
+  };
 };
